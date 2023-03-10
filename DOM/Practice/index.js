3051286e0b8adc3feb1f10de304d4dfd2b9e4c4c
krishna102001 <krishnakantmaurya@icloud.com>
2023-03-10 14:49:54 +0530

    download the resources and move previous file in practice folder and learned all selectorthrough which we can access a html element

---
+++ DOM/Practice/index.js
@@ -0,0 +1,18 @@
+// document.querySelector("h1").innerHTML="good bye";
+
+// we can select an element in different Ways
+
+// 1. by TagName
+// document.getElementsByTagName("")     --> this get the all specifc element which we have given and store that in ARRAY form. 
+
+// 2. by ClassName
+//  document.getElementsByClassName("")  --> this get the all specifc element which we have given and store that in ARRAY form.
+
+// 3. by IdName
+// document.getElementById("")  --> through this we get access to unique Id . Its doesn't store in ARRAY form.
+
+// 4. by querySelector
+// document.querySelector("")   --> through this we can access any single element by specifing its class, Id, or tag.
+
+//5. by querySelectorAll
+// document.querySelectorAll("")   --> through this we can access all element by specifing its class, Id, or tag and store in ARRAY form.
