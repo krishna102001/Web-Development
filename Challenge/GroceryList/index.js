34519d1347048faeef772b857d08f542bb19a6e5
krishna102001 <krishnakantmaurya@icloud.com>
2023-05-05 02:12:01 +0530

    added functionality

---
+++ Challenge/GroceryList/index.js
@@ -0,0 +1,75 @@
+const alert=document.querySelector('.alerts');
+const form=document.querySelector('.grocery-form');
+const list=document.querySelector('.grocery-list');
+const clearBtn=document.querySelector('.clear-btn');
+const grocery=document.getElementById('grocerys');
+const submitBtn=document.querySelector('.btn');
+const container=document.querySelector('.grocery-container');
+let editElement;
+let editFlag=false;
+let editID="";
+function displayAlert(value,text){
+    alert.classList.add(value);
+    alert.innerHTML=text;
+    setTimeout(function(){
+     alert.classList.remove(value);   
+    },2000);
+}
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    // console.log(e);
+    let value=grocery.value;
+    const id =new Date().getTime().toString();
+    console.log(id);
+    if(value !== "" && !editFlag){
+        const element=document.createElement("article");
+        const attr=document.createAttribute("data-id");
+        attr.value=id;
+        element.setAttributeNode(attr);
+        element.classList.add('grocery-item');
+        element.innerHTML= `<p class="title">${value}</p>
+        <div class="btn-container">
+            <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>`;
+        const deleteBtn=element.querySelector('.delete-btn');
+        deleteBtn.addEventListener("click",deleteItem);
+        const editBtn=element.querySelector('.edit-btn');
+        editBtn.addEventListener("click",editItem);
+        list.appendChild(element);
+        displayAlert('success','item added to the list');
+        container.classList.add('show-container');
+        setDefault();
+    }
+    else if(value !=="" && editFlag){
+        editElement.innerHTML=value;
+        displayAlert("success","item Changed");
+        setDefault();
+    }
+    else{
+        displayAlert('danger','please enter the value');
+    }
+})
+function deleteItem(){
+    console.log('deleted');
+}
+function editItem(e){
+    // console.log('edited');
+    const element=e.currentTarget.parentElement.parentElement;
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    grocery.value=editElement;
+    editFlag=true;
+    editID=element.dataset.id;
+    submitBtn.textContent="edit";
+}
+function setDefault(){
+    editFlag=false;
+    grocery.value="";
+    editID="";
+    submitBtn.textContent='submit';
+}
