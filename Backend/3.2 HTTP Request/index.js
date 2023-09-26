ea9b54282ea1b3aa84f1422fef95af7d18de342b
krishna102001 <krishnakantmaurya@icloud.com>
2023-09-27 01:44:45 +0530

    learned GET method to respond the request

---
+++ Backend/3.2 HTTP Request/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    // console.log(req.rawHeaders); it will show the request details
+    // res.send("Hello Express Js!!!");
+    res.send("<h1> Hello World !!");
+});
+
+app.get("/about",(req,res)=>{
+    res.send("<h1>About Me</h1><p>Hello my name is krishna. </p>");
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("<h1>Contact Me</h1><p>+91-6388851459</p>")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on Port ${port}`);
+});
