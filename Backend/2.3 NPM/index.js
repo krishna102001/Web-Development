0b2502d3b3daf675dc275982abe7320ee00fcc01
krishna102001 <krishnakantmaurya@icloud.com>
2023-09-26 19:29:12 +0530

    started learning Node.js

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require("sillyname");
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}`);
+
+
+
+// superheroes modules
+// import superHeroes from "superheroes";
+// const sH = superHeroes.random();
+// console.log(`I am ${sH}`);
