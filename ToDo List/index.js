63d3254daf8e9d3dc92f8ca5c950c77bd928bac3
krishna102001 <krishnakantmaurya@icloud.com>
2023-09-30 04:35:11 +0530

    Creating a to-Do list

---
+++ ToDo List/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let list = [];
+let work = [];
+
+app.use(bodyParser.urlencoded({ extended : true}));
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    list = [];
+    res.render("index.ejs",{weekday : weekday[new Date().getDay()], month : month[new Date().getMonth()]});
+})
+
+app.get("/today",(req,res)=>{
+    res.render("index.ejs",{weekday : weekday[new Date().getDay()], month : month[new Date().getMonth()],list:list});
+})
+
+app.get("/work",(req,res)=>{
+    res.render("index.ejs",{weekday : weekday[new Date().getDay()], month : month[new Date().getMonth()],list:work});
+})
+
+app.post("/submit",(req,res)=>{
+    // console.log(req.body["newItem"]);
+    list.push(req.body["newItem"]);
+    // console.log(list);
+    res.render("index.ejs",{weekday : weekday[new Date().getDay()], month : month[new Date().getMonth()],list:list});
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
