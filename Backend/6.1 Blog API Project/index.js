68b12b35cc248d18c1349b2401dde1f8ed7b7e55
krishna102001 <krishnakantmaurya@icloud.com>
2023-10-06 15:30:37 +0530

    done with logic part

--- Backend/6.1 Blog API Project/index.js
+++ Backend/6.1 Blog API Project/index.js
@@ -41,14 +41,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts",(req,res)=>{
+  res.json(posts);
+})
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id",(req,res)=>{
+  const id = parseInt(req.params.id);
+  const searchPost = posts.find((post)=> post.id === id);
+  if(!searchPost) res.status(404).json({message : "Page not found"});
+  res.json(searchPost);
+})
 
 //CHALLENGE 3: POST a new post
+app.post("/posts",(req,res)=>{
+  const newPost = {
+    id : lastId + 1,
+    title : req.body.title,
+    content : req.body.content,
+    author : req.body.author,
+    date : new Date(),
+  }
+  lastId += 1;
+  posts.push(newPost);
+  res.status(200).json(newPost);
+})
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id",(req,res)=>{
+  const id = parseInt(req.params.id);
+  const post = posts.find((post)=> post.id === id);
+  if (req.body.title) post.title = req.body.title;
+  if (req.body.content) post.content = req.body.content;
+  if (req.body.author) post.author = req.body.author;
+  res.json(post);
+})
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id",(req,res)=>{
+  const id = parseInt(req.params.id);
+  const postIndex = posts.findIndex((post)=> post.id === id);
+  if(postIndex > -1){
+    posts.splice(postIndex,1);
+    res.json({ message: "Post deleted" });
+  }
+  else{
+    res.status(404).json({message : "not found "});
+  }
+})
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
