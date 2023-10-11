09a5b228cbef18d79aa2ec6a04cf2a25ce6d8634
krishna102001 <krishnakantmaurya@icloud.com>
2023-10-11 16:34:03 +0530

    connected the mongodb and worked with the logic parts

--- Blog-with-Database/app.js
+++ Blog-with-Database/app.js
@@ -4,6 +4,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+//connecting mongoose
+mongoose.connect("mongodb://localhost:27017/blogsDB");
+
+//defining schema 
+const blogsSchema = new mongoose.Schema({
+  title : String,
+  content : String
+});
+
+//defining the Model
+const Blog = mongoose.model("Blog",blogsSchema);
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,13 +30,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+// let posts = [];
 
-app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-    });
+app.get("/", async function(req, res){
+    try{
+      const posts = await Blog.find({});
+      // console.log("All Blogs found");
+      // console.log(posts);
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts 
+        });
+    } catch(error){
+      console.log("Blogs Not Found");
+    }
 });
 
 app.get("/about", function(req, res){
@@ -38,31 +59,42 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-
-  posts.push(post);
+  // const post = {
+  //   title: req.body.postTitle,
+  //   content: req.body.postBody
+  // };
+  const blog = new Blog({
+    title : req.body.postTitle,
+    content : req.body.postBody
+  });
+  blog.save();
+  // posts.push(post);
 
   res.redirect("/");
 
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postName", async function(req, res){
+  const requestedTitle =(req.params.postName);
+  // console.log(requestedTitle);
+  // posts.forEach(function(post){
+  //   const storedTitle = _.lowerCase(post.title);
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
-
+  //   if (storedTitle === requestedTitle) {
+  //     res.render("post", {
+  //       title: post.title,
+  //       content: post.content
+  //     });
+  //   }
+  // });
+  try{
+    const post = await Blog.findById({_id : requestedTitle});
+    res.render("post",{ title : post.title, content : post.content});
+    // console.log("successfully found the post");
+  }catch(error){
+    console.log("not found the post");
+    // res.status(404)
+  }
 });
 
 app.listen(3000, function() {
